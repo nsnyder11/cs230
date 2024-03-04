886ec39288751621b9040661ee1f81ca964274dc
nsnyder11 <nps00011@mix.wvu.edu>
2024-03-03 22:44:51 -0500

    lab 8 final

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import React from 'react';
 function App() {
   return (
       <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 001</h2> 
       <p>WVU ID: 800393605</p> 
       <p>Hi I am Nick</p> 
-      <Navbar />
       <Card />
     </div>
   );
